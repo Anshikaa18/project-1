83d14640d04b5d1ef84b8a3f8d0c32755dc717d6
Chetan Patel <patelchetan1736@gmail.com>
2026-07-13 22:10:41 +0530

    Add course_table structure and primary key index
    Added table structure and indexes for course_table.

--- sql/stts_db.sql
+++ sql/stts_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2026 at 06:32 PM
+-- Generation Time: Jul 13, 2026 at 06:39 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.5
 
@@ -23,6 +23,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `course_table`
+--
+
+CREATE TABLE `course_table` (
+  `s.no` int(10) NOT NULL,
+  `course_name` text NOT NULL,
+  `course_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `student_table`
 --
 
@@ -37,6 +49,22 @@ CREATE TABLE `student_table` (
   `semester` int(10) NOT NULL,
   `year` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course_table`
+--
+ALTER TABLE `course_table`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `student_table`
+--
+ALTER TABLE `student_table`
+  ADD PRIMARY KEY (`roll_no`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
